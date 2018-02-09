658c457f207c6e2b885fa196ad184da938f592a7
aiswebmaster <dylan@allisolutions.com>
2018-02-08 23:35:07 -0500

    Front end resources

---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var sitecoreRoot = "C:\\Sitecore\\";
+    var config = {
+        websiteRoot: sitecoreRoot + "\\Website",
+        sitecoreLibraries: sitecoreRoot + "\\Website\\bin",
+        solutionName: "Helix",
+        licensePath: sitecoreRoot + "\\Data\\license.xml",
+        runCleanBuilds: false
+    };
+};
