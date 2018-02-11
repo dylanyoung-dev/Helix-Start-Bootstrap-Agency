b6f2857ab4631a7a16c6743a5836ebfcdd9fcc6c
aiswebmaster <dylan@allisolutions.com>
2018-02-10 23:06:27 -0500

    Glass Base Changes

--- src/Foundation/ORM/code/Models/IGlassBase.cs
+++ src/Foundation/ORM/code/Models/IGlassBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace Helix.Foundation.ORM.Models
 {
     public interface IGlassBase
     {
+        [SitecoreId]
+        Guid Id { get; }
+
+        [SitecoreInfo(SitecoreInfoType.Name)]
+        string ItemName { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.DisplayName)]
+        string DisplayName { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateName)]
+        string TemplateName { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Version)]
+        int Version { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Language)]
+        string Language { get; set; }
     }
 }
