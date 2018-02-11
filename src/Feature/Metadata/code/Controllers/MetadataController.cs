a304540ea7282a8c953ab5d9927702211655f7b6
aiswebmaster <dylan@allisolutions.com>
2018-02-10 22:41:02 -0500

    Meta data Feature Updates

--- src/Feature/Metadata/code/Controllers/MetadataController.cs
+++ src/Feature/Metadata/code/Controllers/MetadataController.cs
@@ -1,4 +1,5 @@
-﻿using Helix.Foundation.ORM.Controllers.Base;
+﻿using Helix.Feature.Metadata.Models;
+using Helix.Foundation.ORM.Controllers.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,12 @@ namespace Helix.Feature.Metadata.Controllers
     {
         public ActionResult RenderMetadata()
         {
-            return View();
+            IMetadata meta = SitecoreContext.GetCurrentItem<IMetadata>();
+
+            if (meta == null)
+                return new EmptyResult();
+
+            return View(meta);
         }
     }
 }
