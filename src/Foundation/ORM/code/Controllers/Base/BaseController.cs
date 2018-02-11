93e3bc5e44d0b71c01b8669beeb51d58f1689690
aiswebmaster <dylan@allisolutions.com>
2018-02-10 21:05:42 -0500

    MetaData Controller Setup, Common Project Definition, ORM Changes

---
+++ src/Foundation/ORM/code/Controllers/Base/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helix.Foundation.ORM.Controllers.Base
+{
+    public class BaseController : GlassController
+    {
+        public BaseController()
+        {
+
+        }
+    }
+}
