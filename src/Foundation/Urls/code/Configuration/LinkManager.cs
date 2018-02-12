ac02bf4a4682ba37ab4a6df976106cff2cc99cc9
Dylan Young <dylan@allisolutions.com>
2018-02-12 18:38:04 -0500

    Added Foundation Urls (to handle naming of items (to be more friendly) and default link manager formatting

---
+++ src/Foundation/Urls/code/Configuration/LinkManager.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Links;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace Helix.Foundation.Urls.Configuration
+{
+    public class LinkManager : LinkProvider
+    {
+        public override string GetItemUrl(Item item, UrlOptions options)
+        {
+            return base.GetItemUrl(item, options);
+        }
+    }
+}
