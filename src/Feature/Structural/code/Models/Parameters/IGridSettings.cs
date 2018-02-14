f59a49ad93cbd236c9de2c6d6c3d5c930946afb0
aiswebmaster <dylan@allisolutions.com>
2018-02-13 22:29:34 -0500

    Starting to build out parts of the Components.. Complete implementation for Section/Container

---
+++ src/Feature/Structural/code/Models/Parameters/IGridSettings.cs
@@ -0,0 +1,17 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helix.Feature.Structural.Models.Parameters
+{
+    public interface IGridSettings
+    {
+        [SitecoreField]
+        string ElementId { get; set; }
+
+        [SitecoreField]
+        string Classes { get; set; }
+    }
+}
