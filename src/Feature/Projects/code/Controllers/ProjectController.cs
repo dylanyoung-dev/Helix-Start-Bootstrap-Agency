e0dfcf7d55b68112e571ea8719687ff16351f190
aiswebmaster <dylan@allisolutions.com>
2018-02-16 22:55:41 -0500

    Added Project Listing Item

--- src/Feature/Projects/code/Controllers/ProjectController.cs
+++ src/Feature/Projects/code/Controllers/ProjectController.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Helix.Foundation.ORM.Controllers.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Helix.Feature.Portfolio.Controllers
 {
-    public class ProjectController
+    public class ProjectController : BaseController
     {
+        public ActionResult Listing()
+        {
+            return View();
+        }
     }
 }
