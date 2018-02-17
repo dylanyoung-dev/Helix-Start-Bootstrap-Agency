8ccfff466e92b76995249e6dceb0f20c797cd16b
aiswebmaster <dylan@allisolutions.com>
2018-02-16 21:58:32 -0500

    Fix Section Header.. Need to Figure out Glass Constants/Model Structure

--- src/Feature/PageContent/code/Models/ISectionHeader.cs
+++ src/Feature/PageContent/code/Models/ISectionHeader.cs
@@ -1,4 +1,5 @@
-﻿using Helix.Foundation.ORM.Models;
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Helix.Foundation.ORM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,13 @@ using System.Threading.Tasks;
 
 namespace Helix.Feature.PageContent.Models
 {
+    [SitecoreType(TemplateId = Constants.TemplateIdString)]
     public interface ISectionHeader : IGlassBase
     {
+        [SitecoreField(FieldName = Constants.TitleName)]
         string Title { get; set; }
 
+        [SitecoreField(FieldName = Constants.DescriptionName)]
         string Description { get; set; }
     }
 }
