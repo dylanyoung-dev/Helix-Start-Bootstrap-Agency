c81e0a8a638253b528ad3ee6d1a477ccf4e7b733
SitecoreMaster <dylan@allisolutions.com>
2018-03-15 11:26:42 -0400

    Added Glass Field/Template Mappings for ProjectCollections (DataSource)

--- src/Feature/Projects/code/Models/Interface/IProjectCollection.cs
+++ src/Feature/Projects/code/Models/Interface/IProjectCollection.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Helix.Feature.Projects.Models.Interface
 {
+    [SitecoreType(TemplateId = Constants.ProjectCollection.TemplateIdString)]
     public interface IProjectCollection
     {
+        [SitecoreField(FieldName = Constants.ProjectCollection.ProjectsName)]
         IEnumerable<IProject> Projects { get; set; }
     }
 }
