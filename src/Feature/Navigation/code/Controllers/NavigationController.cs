fc581fa417aba10bd93c86717515d0899300bdac
SitecoreMaster <dylan@allisolutions.com>
2018-04-14 16:41:36 -0400

    Navigation Restructure

---
+++ src/Feature/Navigation/code/Controllers/NavigationController.cs
@@ -0,0 +1,20 @@
+﻿using Helix.Feature.Navigation.Models;
+using Helix.Foundation.ORM.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helix.Feature.Navigation.Controllers
+{
+    public class NavigationController : BaseController
+    {
+        public ActionResult BasicNavigation()
+        {
+            INavigation navigationDatasource = this.GetDataSourceItem<INavigation>();
+
+            return View(navigationDatasource);
+        }
+    }
+}
---
+++ src/Feature/Navigation/code/Models/INavigationItem.cs
@@ -0,0 +1,19 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helix.Feature.Navigation.Models
+{
+    [SitecoreType(TemplateId = Constants.NavigationItem.TemplateIdString)]
+    public interface INavigationItem
+    {
+        [SitecoreField(FieldName = Constants.NavigationItem.TitleName)]
+        string Title { get; set; }
+
+        [SitecoreField(FieldName = Constants.NavigationItem.LinkName)]
+        Link Link { get; set; }
+    }
+}
