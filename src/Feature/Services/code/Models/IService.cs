12f7a3652bfa35c1941167d236f6413beb03790c
SitecoreMaster <dylan@allisolutions.com>
2018-04-21 10:01:19 -0400

    Added Service Model + Constants

---
+++ src/Feature/Services/code/Models/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+
+namespace Helix.Feature.Services.Models
+{
+    [SitecoreType(TemplateId = Constants.Service.TemplateIdString)]
+    public interface IService
+    {
+        [SitecoreField(FieldName = Constants.Service.TitleName)]
+        string Title { get; set; }
+
+        [SitecoreField(FieldName = Constants.Service.SummaryName)]
+        string Summary { get; set; }
+
+        [SitecoreField(FieldName = Constants.Service.ThumbnailName)]
+        Image Thumbnail { get; set; }
+    }
+}
