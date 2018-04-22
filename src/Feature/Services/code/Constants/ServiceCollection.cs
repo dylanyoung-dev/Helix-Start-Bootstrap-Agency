4559adeaece3bc4882fd1152d533a3197f53a462
SitecoreMaster <dylan@allisolutions.com>
2018-04-22 13:34:11 -0400

    Update ID's for Template and Field

--- src/Feature/Services/code/Constants/ServiceCollection.cs
+++ src/Feature/Services/code/Constants/ServiceCollection.cs
@@ -9,11 +9,11 @@ namespace Helix.Feature.Services.Constants
     public class ServiceCollection
     {
         // Templates
-        public const string TemplateIdString = "{6C04B521-7BEA-438F-98F0-390BDF658704}";
+        public const string TemplateIdString = "{80CC9251-6A08-4008-A18F-5EEC17105A7A}";
         public readonly ID TemplateId = new ID(TemplateIdString);
 
         // Fields
-        public const string ServicesName = "Title";
-        public readonly ID ServicesId = new ID("{625A3008-6BC4-4AEC-AAB5-0198E90DD316}");
+        public const string ServicesName = "Services";
+        public readonly ID ServicesId = new ID("{ABC6E742-74A9-41B2-8B1F-BC2E2956CDFC}");
     }
 }
