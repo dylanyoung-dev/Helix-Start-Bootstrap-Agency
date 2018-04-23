7fa2fd788de2c38c21c0f88a65a2376280dc3b85
SitecoreMaster <dylan@allisolutions.com>
2018-04-23 10:38:59 -0400

    Creating Member Page Type Templates

--- src/Feature/Members/code/Models/IMember.cs
+++ src/Feature/Members/code/Models/IMember.cs
@@ -4,25 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Glass.Mapper.Sc.Fields;
 
 namespace Helix.Feature.Members.Models
 {
     [SitecoreType(TemplateId = Constants.Member.TemplateIdString)]
     public interface IMember : IGlassBase
     {
-        [SitecoreField]
+        [SitecoreField(FieldName = Constants.Member.NameName)]
         string Name { get; set; }
 
-        [SitecoreField]
+        [SitecoreField(FieldName = Constants.Member.PositionName)]
         string Position { get; set; }
 
-        [SitecoreField]
+        [SitecoreField(FieldName = Constants.Member.TwitterUrlName)]
         string TwitterUrl { get; set; }
 
-        [SitecoreField]
+        [SitecoreField(FieldName = Constants.Member.FacebookUrlName)]
         string FacebookUrl { get; set; }
 
-        [SitecoreField]
+        [SitecoreField(FieldName = Constants.Member.LinkedInUrlName)]
         string LinkedInUrl { get; set; }
+
+        [SitecoreField(FieldName = Constants.Member.ThumbnailName)]
+        Image Thumbnail { get; set; }
     }
 }
