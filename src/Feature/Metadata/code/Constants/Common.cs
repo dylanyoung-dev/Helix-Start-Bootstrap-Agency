3ba1590caea47212f862218cf4fb2d6cefd481e1
SitecoreMaster <dylan@allisolutions.com>
2018-04-23 10:27:52 -0400

    Create Metadata Constants + Update Metadata on Pages

---
+++ src/Feature/Metadata/code/Constants/Common.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helix.Feature.Metadata.Constants
+{
+    public class Common
+    {
+        public const string RenderingId = "{D677DB44-8E42-4BC9-B02B-965DB507EDCB}";
+    }
+}
