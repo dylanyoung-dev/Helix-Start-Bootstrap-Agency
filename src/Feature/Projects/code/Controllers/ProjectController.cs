44a4620b3fb8400434c99714a9226e6a1ef361b5
SitecoreMaster <dylan@allisolutions.com>
2018-04-27 22:25:06 -0400

    Fixing Build Errors

--- src/Feature/Projects/code/Controllers/ProjectController.cs
+++ src/Feature/Projects/code/Controllers/ProjectController.cs
@@ -1,4 +1,4 @@
-﻿using Helix.Feature.Projects.Models.Interface;
+﻿using Helix.Feature.Projects.Models;
 using Helix.Foundation.ORM.Controllers.Base;
 using System;
 using System.Collections.Generic;
